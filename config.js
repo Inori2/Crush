c45804d958dac1bfb6afe9eedec61f780acfc2eb
Inori2 <88781873+Inori2@users.noreply.github.com>
2022-04-11 22:36:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "For Close Friend",
+    introTitle: 'For Thẻo Cruise',
+    introDesc: `This webpage is download from chrome and get change a few think from your "close friend"`,
+    btnIntro: '^^Get started^^',
+    title: 'Do you have feeling for me 🥰',
+    desc: 'Answer this to move forward the next question ',
+    btnYes: 'Yes i do <33',
+    btnNo: 'In your dream :((',
+    question: 'Anh có thể từ closefriend thành boyfriend được hem',
+    btnReply: 'Sent',
+    reply: 'Anh đã phù phép đoạn này rồi nên không từ chối được đâu :)))',
+    mess: 'haha thời gian tới, để anh lo :)))',
+    messDesc: 'Thanks you for coming',
+    btnAccept: 'Luv ya <3',
+    messLink: 'https://www.facebook.com/messages/t/100009209916437' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
